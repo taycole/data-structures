3821acdf7ff29c189b3758d0f5958624ba1f28bc
Taylor Cole <taylor.cole.385@gmail.com>
2026-06-04 15:11:09 -0700

    Made a generic macro version for fun. Renamed some files

---
+++ generic_da_macros.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <errno.h>
+
+// Gets the number of elements in an array
+#define NELEMS(arr) (sizeof(arr) / sizeof(*arr))
+
+/**
+ * Creates an empty dynamic array on the heap with default capacity 4
+ */
+#define da_init(Type)\
+({\
+    Type* new_arr = malloc(sizeof(Type));\
+    new_arr->size = 0;\
+    new_arr->capacity = 4;\
+    new_arr->data = malloc(new_arr->capacity * sizeof(*new_arr->data));\
+    new_arr;\
+})
+
+#define da_free(arr)\
+do {\
+    free(arr->data);\
+    free(arr);\
+} while(0)
+
+/**
+ * Resize the array when size meets or exceeds current capacity
+ */
+#define da_resize(array, new_capacity)\
+do {\
+    if (new_capacity > 0 && new_capacity >= array->size) {\
+        array->data = realloc(array->data, new_capacity * sizeof(*array->data));\
+        array->capacity = new_capacity;\
+    }\
+} while(0)
+
+/**
+ * Adds a new value at the end of the dynamic array
+ */
+#define da_append(array, val)\
+do {\
+    if (array->size == array->capacity)\
+	da_resize(array, array->capacity * 2);\
+    array->data[array->size++] = val;\
+} while(0)
+
+/**
+ * Converts a static array to a dynamic array
+ */
+#define da_convert(Type, arr, len)\
+({\
+    Type* da_arr = da_init(Type);\
+    for (int i = 0; i < len; i++)\
+	da_append(da_arr, arr[i]);\
+    da_arr;\
+})
+
+/**
+ * Inserts a value at the specified index
+ */
+#define da_insert_at_index(da_arr, index, val)\
+do {\
+    if (index >= 0 && index <= da_arr->size) {\
+        if (da_arr->size == da_arr->capacity)\
+    	    da_resize(da_arr, da_arr->capacity * 2);\
+        for (int i = da_arr->size; i > index; i--) {\
+    	    da_arr->data[i] = da_arr->data[i-1];\
+        }\
+        da_arr->data[index] = val;\
+        da_arr->size++;\
+    }\
+} while(0)
+
+/**
+ * Remove the value at the specified index
+ */
+#define da_remove_at_index(arr, index)\
+do {\
+    if (index >= 0 && index <= arr->size) {\
+	if (arr->capacity / 4 > arr->size) {\
+	    if (arr->size < 5 && arr->capacity > 10)\
+		da_resize(arr, 10);\
+	    else\
+		da_resize(arr, arr->size * 2);\
+	}\
+	if (arr->size == 1) {\
+	    arr->size--;\
+	}\
+	else {\
+	    for (int i = index; i < arr->size - 1; i++)\
+	        arr->data[i] = arr->data[i+1];\
+	    arr->size--;\
+	}\
+    }\
+} while(0)
+
+/**
+ * Get a slice!! Returns a new array slice of the given size starting at the
+ * given index.
+ */
+#define da_slice(Type, arr, start, slice_size)\
+({\
+    Type* new_arr = da_init(Type);\
+    if ((start < 0 || start >= arr->size) ||\
+	(slice_size < 0 || slice_size > arr->size - start))\
+	new_arr->data = NULL;\
+    else {\
+        for (int i = 0; i < slice_size; i++)\
+	    da_insert_at_index(new_arr, i, arr->data[start+i]);\
+    }\
+    new_arr;\
+})
+
+/**
+ * This one slices from between a start and end.
+ */
+#define da_slice_sv(Type, arr, start, end)\
+    (da_slice(Type, arr, start, end - start + 1))
+
+
+/**
+ * Creates an array of values derived from the given function
+ */
+#define da_map(Type, arr, func)\
+({\
+    Type* new_arr = da_init(Type);\
+    for (int i = 0; i < arr->size; i++)\
+	da_append(new_arr, func(arr->data[i]));\
+    new_arr;\
+})
+
+/**
+ * Creates a new array of values that pass a filter function
+ */
+#define da_filter(Type, arr, func)\
+({\
+    Type* new_arr = da_init(Type);\
+    for (int i = 0; i < arr->size; i++) {\
+	if (func(arr->data[i]) == true)\
+	    da_append(new_arr, arr->data[i]);\
+    }\
+    new_arr;\
+})
