0fe1a5cf66600cde3b40aaa4b0e5b8ee371321e6
Taylor Cole <taylor.cole.385@gmail.com>
2026-06-12 15:27:38 -0700

    Changed name "value" to "key"

--- int_bt.c
+++ int_bt.c
@@ -17,7 +17,7 @@
 #define LRN   2
 
 typedef struct BT_Node {
-    int value;
+    int key;
     struct BT_Node* left;
     struct BT_Node* right;
 } BT_Node;
@@ -25,6 +25,18 @@ typedef struct BT_Node {
 typedef struct BT {
     BT_Node* root;
 } BT;
+
+typedef struct BT_Queue_DA {
+    size_t size;
+    size_t capacity;
+    BT_Node** data;
+} BT_Queue_DA;
+
+typedef struct Int_Queue_DA {
+    size_t size;
+    size_t capacity;
+    int* data;
+} Int_Queue_DA;
 
 /**
  * Initialize an empty tree
@@ -37,19 +49,19 @@ BT* bt_init()
 }
 
 /**
- * Create a node with the given value
+ * Create a node with the given key
  */
 BT_Node* bt_node_init(int val)
 {
     BT_Node* new_node = malloc(sizeof(BT_Node));
-    new_node->value = val;
+    new_node->key = val;
     new_node->left = NULL;
     new_node->right = NULL;
     return new_node;
 }
 
 /**
- * Add a node with a value to the binary tree
+ * Add a node with a key to the binary tree
  */
 void bt_add(BT* bt, int val)
 {
@@ -62,7 +74,7 @@ void bt_add(BT* bt, int val)
 
     while (curr) {
 	parent = curr;
-	if (val < curr->value)
+	if (val < curr->key)
 	    curr = curr->left;
 	else
 	    curr = curr->right;
@@ -70,7 +82,7 @@ void bt_add(BT* bt, int val)
 
     BT_Node* new_node = bt_node_init(val);
 
-    if (val < parent->value)
+    if (val < parent->key)
 	parent->left = new_node;
     else
 	parent->right = new_node;
@@ -85,7 +97,7 @@ void bt_remove_no_subtrees(BT* bt, BT_Node* r_parent, BT_Node* r_node)
     if (r_parent == NULL)
 	bt->root = NULL;
     // Remove node is to the left
-    else if (r_node->value < r_parent->value) 
+    else if (r_node->key < r_parent->key)
 	r_parent->left = NULL;
     // Remove node is to the right
     else
@@ -102,18 +114,18 @@ void bt_remove_one_subtree(BT* bt, BT_Node* r_parent, BT_Node* r_node)
     // Determine which child
     BT_Node* child;
     if (r_node->left == NULL)
-        child = r_node->right;
+	child = r_node->right;
     else
 	child = r_node->left;
 
     // Replace node to be removed with its child
     if (r_parent == NULL)
 	bt->root = child;
-    else if (r_node->value < r_parent->value)
+    else if (r_node->key < r_parent->key)
 	r_parent->left = child;
     else
 	r_parent->right = child;
-    
+
     free(r_node);
 }
 
@@ -151,11 +163,10 @@ void bt_remove_two_subtrees(BT* bt, BT_Node* r_parent, BT_Node* r_node)
 	s.parent->left = s.successor->right;
 	s.successor->right = r_node->right;
     }
-
     // Update removed node's parent to point to new node. If root, set as root.
     if (r_parent == NULL)
 	bt->root = s.successor;
-    else if (r_node->value < r_parent->value)
+    else if (r_node->key < r_parent->key)
 	r_parent->left = s.successor;
     else
 	r_parent->right = s.successor;
@@ -164,19 +175,18 @@ void bt_remove_two_subtrees(BT* bt, BT_Node* r_parent, BT_Node* r_node)
 }
 
 /**
- * Removes a value from the tree. Returns true if successful, false otherwise
+ * Removes a key from the tree. Returns true if successful, false otherwise
  */
 bool bt_remove(BT* bt, int val)
 {
     BT_Node *parent = NULL, *curr_node = bt->root;
-    while (curr_node && curr_node->value != val) {
+    while (curr_node && curr_node->key != val) {
 	parent = curr_node;
-	if (val < curr_node->value)
+	if (val < curr_node->key)
 	    curr_node = curr_node->left;
 	else
 	    curr_node = curr_node->right;
     }
-
     // Search failed
     if (curr_node == NULL)
 	return false;
@@ -194,6 +204,54 @@ bool bt_remove(BT* bt, int val)
     return true;
 }
 
+bool bt_contains(BT* bt);
+
+
+/**
+ * Recursive helper function to traverse a tree and store keys in a queue.
+ */
+void bt_rec_traverse(BT_Node* node, int mode, Int_Queue_DA* q)
+{
+    if (mode == NLR) {
+	if (node) {
+	    da_enqueue(q, node->key);
+	    bt_rec_traverse(node->left, mode, q);
+	    bt_rec_traverse(node->right, mode, q);
+	}
+    }
+    else if (order == LNR) {
+	if (node) {
+	    bt_rec_traverse(node->left, mode, q);
+	    da_enqueue(q, node->key);
+	    bt_rec_traverse(node->right, mode, q);
+	}
+    }
+    else if (order == LRN) {
+	if (node) {
+	    bt_rec_traverse(node->left, mode, q);
+	    bt_rec_traverse(node->right, mode, q);	    
+            da_enqueue(q, node->key);
+	}
+    }
+}
+
+/**
+ * Performs a recursive traversal of the tree, stores keys in a queue and
+ * returns the queue. Can perform preorder, inorder, or postorder traversal
+ * based on specified option. Options have macro names to help.
+ *
+ * @param: int mode: 0 = NLR = preorder
+ *                   1 = LNR = inorder
+ *                   2 = LRN = postorder
+ *
+ */
+Int_Queue_DA* bt_recursive_traversal(BT* bt, int mode)
+{
+    Int_Queue_DA* q = da_init(Int_Queue_DA);
+    bt_rec_traverse(bt->root, mode, q);
+    return q;
+}
+
 
 
 // ----------- Functions to find height/width, print tree ---------------------
@@ -206,7 +264,7 @@ void rec_get_height(BT_Node* node, int curr_h, int* h)
     if (node != NULL) {
 	rec_get_height(node->left, curr_h+1, h);
 	rec_get_height(node->right, curr_h+1, h);
-        *h = (curr_h > *h)? curr_h : *h;
+	*h = (curr_h > *h)? curr_h : *h;
     }
 }
 
@@ -220,28 +278,23 @@ int bt_get_height(BT* bt)
     return h;
 }
 
-typedef struct Queue_DA {
-    size_t size;
-    size_t capacity;
-    BT_Node** data;
-} Queue_DA;
-
 /**
  * Finds the max width of the tree using a BFS traversal with a queue
  */
 int bt_get_width(BT* bt)
 {
     int max_w = 1;
-    Queue_DA* q = da_init(Queue_DA);
+    BT_Queue_DA* q = da_init(BT_Queue_DA);
     da_enqueue(q, bt->root);
 
     while (q->size != 0) {
 	max_w = (q->size > max_w)? q->size : max_w;
 	BT_Node* node = da_dequeue(q);
-	
+
 	if (node->left) da_enqueue(q, node->left);
 	if (node->right) da_enqueue(q, node->right);
     }
+    da_free(q);
     return max_w;
 }
 
@@ -250,13 +303,13 @@ typedef struct Arr_2D {
 } Arr_2D;
 
 /**
- * Puts values of tree into a matrix in order to print. Uses a DFS search.
+ * Puts keys of tree into a matrix in order to print. Uses a DFS search.
  */
 void rec_fill_matrix(BT_Node* node, Arr_2D* matrix, int row, int l_col, int r_col)
 {
     if (node) {
 	int mid_col = (l_col + r_col) / 2;
-	matrix->data[row][mid_col] = node->value;
+	matrix->data[row][mid_col] = node->key;
 	rec_fill_matrix(node->left, matrix, row+1, l_col, mid_col-1);
 	rec_fill_matrix(node->right, matrix, row+1, mid_col+1, r_col);
     }
@@ -290,7 +343,7 @@ void bt_print_queue(BT* bt)
     free(matrix);
 }
 
-
+// ----------------------------------------------------------------------------
 
 int main()
 {
@@ -320,14 +373,14 @@ int main()
 
 
 ///**
-// * Recursive helper to push binary tree values onto linked-list stack in
+// * Recursive helper to push binary tree keys onto linked-list stack in
 // * order (LNR)
 // */
 //void rec_convert_stack(BT_Node* node, Int_Node* stack, int order)
 //{
 //    if (order == NLR) {
 //        if (node) {
-//            sll_push(stack, node->value);
+//            sll_push(stack, node->key);
 //	    rec_convert_stack(node->left, stack, order);
 //	    rec_convert_stack(node->right, stack, order);
 //        }
@@ -335,7 +388,7 @@ int main()
 //    else if (order == LNR) {
 //        if (node) {
 //	    rec_convert_stack(node->left, stack, order);
-//            sll_push(stack, node->value);
+//            sll_push(stack, node->key);
 //	    rec_convert_stack(node->right, stack, order);
 //        }
 //    }
@@ -343,7 +396,7 @@ int main()
 //        if (node) {
 //	    rec_convert_stack(node->left, stack, order);
 //	    rec_convert_stack(node->right, stack, order);
-//            sll_push(stack, node->value);
+//            sll_push(stack, node->key);
 //        }
 //    }
 //    else
