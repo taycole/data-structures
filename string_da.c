212bbcaeaca0176f0e3af5c5e4e72dbbd3e64f14
Taylor Cole <taylor.cole.385@gmail.com>
2026-08-08 18:02:29 -0700

    formatting

--- string_da.c
+++ string_da.c
@@ -64,7 +64,7 @@ void str_da_resize(Str_DA* arr, int new_capacity)
 {
     // Must be a positive integer greater than current size
     if (new_capacity <= 0 || new_capacity < arr->size) {
-	return;
+        return;
     }
 
     arr->data = realloc(arr->data, new_capacity * sizeof(*arr->data));
@@ -77,7 +77,7 @@ void str_da_resize(Str_DA* arr, int new_capacity)
 void str_da_append(Str_DA* arr, char val)
 {
     if (arr->size == arr->capacity)
-	str_da_resize(arr, arr->capacity * 2);
+        str_da_resize(arr, arr->capacity * 2);
 
     arr->data[arr->size++] = val;
 }
@@ -91,7 +91,7 @@ Str_DA* str_da_set(char* arr, size_t len)
 
     // Copy values
     for (int i = 0; i < len; i++) {
-	str_da_append(str_da_arr, arr[i]);
+        str_da_append(str_da_arr, arr[i]);
     }
     return str_da_arr;
 }
@@ -103,15 +103,15 @@ void str_da_insert_at_index(Str_DA* arr, int index, char val)
 {
     // Bounds check
     if (index < 0 || index > arr->size)
-	return;
+        return;
 
     // If at capacity
     if (arr->size == arr->capacity)
-	str_da_resize(arr, arr->capacity * 2);
+        str_da_resize(arr, arr->capacity * 2);
 
     // Shift values to the right at insert location
     for (int i = arr->size; i > index; i--) {
-	arr->data[i] = arr->data[i-1];
+        arr->data[i] = arr->data[i-1];
     }
     arr->data[index] = val;
     arr->size++;
@@ -124,25 +124,25 @@ void str_da_remove_at_index(Str_DA* arr, int index)
 {
     // Bounds check
     if (index < 0 || index > arr->size)
-	return;
+        return;
 
     // Shrinks array if size less than 1/4 capacity
     if (arr->capacity / 4 > arr->size) {
-	// If size less than 5, defaults to 10 capacity
-	if (arr->size < 5 && arr->capacity > 10)
-	    str_da_resize(arr, 10);
-	else
-	    str_da_resize(arr, arr->size * 2);
+        // If size less than 5, defaults to 10 capacity
+        if (arr->size < 5 && arr->capacity > 10)
+            str_da_resize(arr, 10);
+        else
+            str_da_resize(arr, arr->size * 2);
     }
     // If only one element, remove it and return
     if (arr->size == 1) {
-	arr->size--;
-	return;
+        arr->size--;
+        return;
     }
 
     // Remove value by shifting to the left and overriding
     for (int i = index; i < arr->size - 1; i++)
-	arr->data[i] = arr->data[i+1];
+        arr->data[i] = arr->data[i+1];
 
     arr->size--;
 }
@@ -155,13 +155,13 @@ Str_DA* str_da_slice(Str_DA* arr, int start, int size)
 {
     // Bounds check
     if ((start < 0 || start >= arr->size) ||
-	(size < 0 || size > arr->size - start))
-	return NULL;
+        (size < 0 || size > arr->size - start))
+        return NULL;
 
     Str_DA* new_arr = str_da_init();
 
     for (int i = 0; i < size; i++)
-	str_da_insert_at_index(new_arr, i, arr->data[start+i]);
+        str_da_insert_at_index(new_arr, i, arr->data[start+i]);
 
     return new_arr;
 }
@@ -180,7 +180,7 @@ Str_DA* str_da_slice_sv(Str_DA* arr, int start, int end)
 Str_DA* str_da_cat(Str_DA* dest, const Str_DA* src)
 {
     for(int i = 0; i < src->size; i++) {
-	str_da_append(dest, src->data[i]);
+        str_da_append(dest, src->data[i]);
     }
     return dest;
 }
@@ -190,7 +190,7 @@ Str_DA* str_da_dup(const Str_DA* src)
     Str_DA* new_str = str_da_init();
 
     for(int i = 0; i < src->size; i++) {
-	str_da_append(new_str, src->data[i]);
+        str_da_append(new_str, src->data[i]);
     }
     return new_str;
 }
@@ -198,7 +198,7 @@ Str_DA* str_da_dup(const Str_DA* src)
 bool str_da_cmp(const Str_DA* str_1, const Str_DA* str_2)
 {
     if (str_1->size != str_2->size)
-	return false;
+        return false;
 
     for (int i = 0; i < str_1->size; i++) {
 	if (str_1->data[i] != str_2->data[i])
